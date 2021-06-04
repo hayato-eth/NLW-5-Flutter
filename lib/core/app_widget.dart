119f77a292e32a4def175debcdeb2112ae1d9f49
Vinicius Rocha <vinicius.r.s@globo.com>
2021-06-03 21:24:26 -0300

    feat: add navigator in splash

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-
-import 'package:DevQuiz/challenge/challenge_page.dart';
-import 'package:DevQuiz/home/home_page.dart';
 import 'package:DevQuiz/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "DevQuiz", home: HomePage());
+    return MaterialApp(title: "DevQuiz", home: SplashPage());
   }
 }
